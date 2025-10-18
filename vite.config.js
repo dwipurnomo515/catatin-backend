195a8e5078625e039d72a65019ace10ee8de6b4a
Dwipurnomo5151 <dwipurnomo5151@gmail.com>
2025-10-18 19:05:52 +0700

    edit vercel

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,21 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
         tailwindcss(),
     ],
+
+    // START KOREKSI PENTING
+    build: {
+        // Mengatur base path untuk aset yang di-generate.
+        // Ini memastikan aset diload dari root domain di Vercel.
+        base: "/",
+    },
+    // END KOREKSI PENTING
 });
